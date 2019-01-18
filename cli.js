c394dc7bdda3fe9c09c60e9cb3c1dda21f498de4
Shawn Allen <shawn.allen@github.com>
2019-01-18 15:21:26 -0800

    rework cli args, default config

--- cli.js
+++ cli.js
@@ -1,13 +1,48 @@
 #!/usr/bin/env node
 const cosmiconfig = require('cosmiconfig')
-const opts = require('yargs')
-  .option('dry-run', {type: Boolean})
-  .argv
-
-const pkg = require('./package.json')
 const sync = require('.')
 
+const opts = require('yargs')
+  .usage('$0 [options] [<output directory>]')
+  .option('from', {
+    type: 'string',
+    array: false,
+    default: sync.defaults.from
+  })
+  .option('packages', {
+    type: 'string',
+    alias: 'p',
+    default: sync.defaults.packages
+  })
+  .option('files', {
+    type: String,
+    array: false,
+    alias: 'f',
+    default: sync.defaults.files
+  })
+  .option('dry-run', {
+    type: 'boolean',
+    alias: 'n'
+  })
+  .alias('help', 'h')
+  .argv
+
+if (opts.help) {
+  return yargs.showHelp()
+}
+
+const args = opts._
+if (args.length === 2) {
+  opts.from = args.shift()
+}
+if (args.length === 1) {
+  opts.to = args.shift()
+}
+
+
+const pkg = require('./package.json')
 const explorer = cosmiconfig(pkg.name)
+
 explorer.search()
   .then(cosmic => {
     if (cosmic) {
